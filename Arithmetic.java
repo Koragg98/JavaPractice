a5cee807f1c7651bcb6e399269b452c4d342599b
CodeOrganizer <irakib3698@gmail.com>
2024-08-22 22:50:53 +0530

    new

---
+++ Arithmetic.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+import java.util.*;
+
+
+public class Arithmetic {
+    public static void main(String[] args) {
+        float base,height,area;
+        Scanner s=new Scanner(System.in);
+        base=s.nextFloat();
+    }
+    
+}

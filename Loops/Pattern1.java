2fbb34dd30dd0e4679092a2dde72cb9bd04d89e0
Rakib Islam <109210735+Koragg98@users.noreply.github.com>
2024-08-31 19:34:21 +0530

    Create Pattern1.java

---
+++ Loops/Pattern1.java
@@ -0,0 +1,54 @@
+package loops;
+
+/**
+ *
+ * @author iraki
+ */
+public class Pattern1 {
+
+    
+    public static void main(String[] args) {
+        
+        int count=1;
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=5;j++)
+            {
+                System.out.printf("%02d ",count);
+                count++;
+            }
+            System.out.println("");
+        }
+//        for(int i=1;i<=5;i++)
+//        {
+//            for(int j=1;j<=5;j++)
+//            {
+//                System.out.print(i+j+" ");
+//                
+//            }
+//            System.out.println("");
+//        }
+
+
+//        for(int i=1;i<=5;i++)
+//        {
+//            for(int j=1;j<=5;j++)
+//            {
+//                System.out.print(i+" ");
+//            }
+//            System.out.println("");
+//        }
+        
+
+
+//        for(int i=1;i<=5;i++)
+//        {
+//            for(int j=1;j<=5;j++)
+//            {
+//                System.out.print(j+" ");
+//            }
+//            System.out.println("");
+//        }
+    }
+
+}

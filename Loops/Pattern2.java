ac6bc4739b2ecec4fde8622952143eae0a006d2c
Rakib Islam <109210735+Koragg98@users.noreply.github.com>
2024-08-31 19:35:57 +0530

    Create Pattern2.java

---
+++ Loops/Pattern2.java
@@ -0,0 +1,48 @@
+package loops;
+
+/**
+ *
+ * @author iraki
+ */
+public class Pattern2 {
+
+    
+    public static void main(String[] args) {
+        
+        
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=6-i;j++)
+            {
+                System.out.print(j+" ");
+              
+            }
+            System.out.println("");
+        }
+        
+        
+        
+//        int count=1;
+//        for(int i=1;i<=5;i++)
+//        {
+//            for(int j=1;j<=i;j++)
+//            {
+//                System.out.print(count+" ");
+//                count++;
+//            }
+//            System.out.println("");
+//        }
+
+
+
+//        for(int i=1;i<=5;i++)
+//        {
+//            for(int j=1;j<=i;j++)
+//            {
+//                System.out.print(j+" ");
+//            }
+//            System.out.println("");
+//        }
+    }
+
+}
